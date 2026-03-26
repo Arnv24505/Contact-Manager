0d48c1eafd7b9baaafcaff8bdc9034d13f0ba9b4
Arnv24505 <github24505@gmail.com>
2026-03-26 20:41:54 +0530

    made a correction

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://contact-manager-1-jm72.onrender.com',
+  baseURL: 'https://contact-manager-1-jm72.onrender.com/api',
 });
 
 export default API;
