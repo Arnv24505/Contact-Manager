0b53ca9ada74009bc6298f0e2b0ff21a6766fe03
Arnv24505 <github24505@gmail.com>
2026-03-26 20:38:52 +0530

    both the servers are connected

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://contact-manager-1-jm72.onrender.com',
 });
 
 export default API;
